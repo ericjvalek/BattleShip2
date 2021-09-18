790ca33da6404e459386fbeafa02986d8a36ed21
Eric Valek <ericjvalekgmail.com>
2021-09-18 12:06:51 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    public class Position //Creating class to pinpoint locations for attacks
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+    }
+
+    
+    
+    
+
+    
+
+
+    
+
+    
+
+}
+
+
